0c16b283d1e4ad8387080ec164a196fc5aac277b
carl <mzfly6543@163.com>
2018-06-22 14:39:22 +0800

    sign in function for ajax

--- front/src/js/auth.js
+++ front/src/js/auth.js
@@ -9,30 +9,18 @@
 //     });
 // });
 
-$(function () {
-    $(".switch").click(function () {
-        var scrollWrapper = $(".scroll-wrapper");
-        // currentLeft含有px的字符串,
-        // 判断大小就需要解析为整形
-        var currentLeft = scrollWrapper.css("left");
-        currentLeft = parseInt(currentLeft);
-        if(currentLeft < 0){
-            scrollWrapper.animate({"left": "0"});
-        }else{
-            scrollWrapper.animate({"left": "-400px"});
-        }
-    });
-});
-
 // 构造函数
 function Auth() {
     var self = this;
     self.maskWrapper = $(".mask-wrapper");
+    self.scrollWrapper = $(".scroll-wrapper");
 }
 
 Auth.prototype.run = function () {
     var self = this;
     self.listenShowHideEvent();
+    self.listenSwitchEvent();
+    self.listenSigninEvent();
 };
 
 Auth.prototype.showEvent = function () {
@@ -50,20 +38,72 @@ Auth.prototype.listenShowHideEvent = function () {
     var signinBtn = $(".signin-btn");
     var signupBtn = $(".signup-btn");
     var closeBtn = $(".close-btn");
-    var scrollWrapper = $(".scroll-wrapper");
     signinBtn.click(function () {
         self.showEvent();
-        scrollWrapper.css({"left": 0});
+        self.scrollWrapper.css({"left": 0});
     });
 
     signupBtn.click(function () {
         self.showEvent();
-        scrollWrapper.css({"left": -400});
+        self.scrollWrapper.css({"left": -400});
     });
 
     closeBtn.click(function () {
         self.hideEvent();
     });
+};
+
+Auth.prototype.listenSwitchEvent = function () {
+    var self = this;
+    var switcher = $(".switch");
+    switcher.click(function () {
+        // currentLeft含有px的字符串,
+        // 判断大小就需要解析为整形
+        var currentLeft = self.scrollWrapper.css("left");
+        currentLeft = parseInt(currentLeft);
+        if(currentLeft < 0){
+            self.scrollWrapper.animate({"left": "0"});
+        }else{
+            self.scrollWrapper.animate({"left": "-400px"});
+        }
+    });
+};
+
+Auth.prototype.listenSigninEvent = function () {
+    var self = this;
+    var signinGroup = $(".signin-group");
+    var telephoneInput = signinGroup.find("input[name='telephone']");
+    var passwordInput = signinGroup.find("input[name='password']");
+    var rememberInput = signinGroup.find("input[name='remember']");
+
+    var submitBtn = signinGroup.find(".submit-btn");
+    submitBtn.click(function () {
+        var telephone = telephoneInput.val();
+        var password = passwordInput.val();
+        // 拿到勾选框
+        var remember = rememberInput.prop("checked");
+
+        cpajax.post({
+            'url': '/account/login/',
+            'data': {
+                'telephone': telephone,
+                'password': password,
+                // 有remember给1否则给0
+                'remember': remember?1:0
+            },
+            // 成功执行的回调函数
+            'success': function (result) {
+                if(result['code'] == 200){
+                    self.hideEvent();
+                    window.location.reload();
+                }
+            },
+            // 失败执行的回调函数
+            'fail': function (error) {
+                console.log(error);
+            }
+        });
+    })
 };
 
 $(function () {
